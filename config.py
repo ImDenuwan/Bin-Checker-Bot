b38f95ad1326d3eb59f87841da4745daf373658b
Chamindu Denuwan <90548223+ImDenuwan@users.noreply.github.com>
2022-03-14 15:14:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+from os import path, getenv
 
+class Config:
+    API_ID = int(getenv("API_ID", "0112234"))
+    API_HASH = getenv("API_HASH", "abcdefg")
+    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+
+config = Config()
