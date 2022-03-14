7dc6bed3f2495bd9935787766cf8f3ade3bab6e3
Chamindu Denuwan <90548223+ImDenuwan@users.noreply.github.com>
2022-03-14 15:27:08 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,12 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from configs import config
+
+Bot = Client(
+    ":memory:",
+    api_hash=config.API_ID,
+    api_id= config.API_HASH,
+    bot_token=config.BOT_TOKEN
+)
 
