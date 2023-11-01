e3308b778067c0cf3592ff662f59e469061af1d8
JrzGerard <146796910+JrzGerard@users.noreply.github.com>
2023-11-01 13:18:42 -0600

    Actualizar configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    API_ID = int(getenv("API_ID", "21591888"))
+    API_HASH = getenv("API_HASH", "d3a40a4f3cd858f80a6ef1659cc4df48")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6552338134:AAEsmnqmFNROXtdThB25dV4JBV6942-Q90A")
 
 config = Config()
